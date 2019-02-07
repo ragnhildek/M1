17ae2e713c9a827db23cbe544103672cdce88714
Ashild <ashild.bekken@gmail.com>
2019-02-07 11:04:23 +0100

    message

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String datafile = "1D2R1V.txt";
+		String datafile = "1D10R1V.txt";
 		//ArrayList<PickupNode> pickupnodes = new ArrayList<PickupNode>();
 		//ArrayList<DeliveryNode> deliverynodes = new ArrayList<DeliveryNode>();
 		ArrayList<Node> nodes = new ArrayList<Node>();
